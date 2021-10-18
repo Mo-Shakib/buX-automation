96bdea21e5ebe5a336c99ff4aa1f48cdebb0cd8d
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-10-18 21:43:47 +0600

    exception addad

--- course_enrollment_automation.py
+++ course_enrollment_automation.py
@@ -4,10 +4,26 @@ import os, time
 # adding the chromedriver folder location to PATH variable
 os.environ['PATH'] += r"E:/GitHub/buX-automation"
 
+print('       _________ ____              ____    ____ ___.    \n'
+'      /   _____/ |  |__   _____    |  | __ |__| \_ |__  \n'
+'      \_____  \  |  |  \  \__  \   |  |/ / |  |  | __ \ \n'
+'      /        \ |   Y  \  / __ \_ |    <  |  |  | \_\ \ \n'
+'     /_______  / |___|  / (____  / |__|_ \ |__|  |___  /\n'
+'             \/      \/        \/       \/           \/ \n')
+
+print ("[+] ABOUT SCRIPT:")
+print ("[-] With this script, you can automatically enroll to multiple buX courses.")
+print ("[-] Version: 1.0.0")
+print ("--------")
+print ("[+] THIS SCRIPT CODDED BY SHAKIB") 
+print ("[-] SITE: www.mo-shakib.me") 
+print ("--------")
+
+
 # User info:
-bux_email = input('Enter your email address for buX: ')
-bux_password = input('Password: ')
-courses = list(input('Enter courses code you want to enroll (separated by comma):').split(','))
+bux_email = input('[+] Enter your email address for buX: ')
+bux_password = input('[+] Password: ')
+courses = list(input('[+] Enter courses code you want to enroll (separated by comma): ').split(','))
 
 driver = webdriver.Chrome()
 
@@ -20,17 +36,22 @@ def buX_login(email, password):
     loginButton = driver.find_element_by_xpath('//*[@id="login"]/button')
     loginButton.click()
     time.sleep(10)
-    print('[] - Login Successfull ')
+    print('[+] - Login Successfull ')
 
 def enroll_course(course_code):
+    print(f'[*] Getting: {course_code.upper()}')
     driver.get(f'https://bux.bracu.ac.bd/courses/course-v1:buX+{course_code.upper()}+2021_Fall/about')
     enroll_Course = driver.find_element_by_xpath('//*[@id="content"]/section/header[2]/div/div[2]/h1/div/a')
     enroll_Course.click()
     time.sleep(10)
+    ptint(f'[=] Enroll complete: {course_code}')
     
     
 buX_login(bux_email,bux_password)
 for i in courses:
-    enroll_course(i.strip())
+    try:
+        enroll_course(i.strip())
+    except:
+        print('[#] Got some error!!')
     
 driver.quit()
